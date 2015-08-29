d904a88be53f63edc87f8da931f2a376deaf4c2b
Mark Servilla <mark.servilla@gmail.com>
2015-08-28 20:41:14 -0600

    Add example for interating over objects.

--- src/examples/file_iterator.py
+++ src/examples/file_iterator.py
@@ -14,8 +14,31 @@
 
 __author__ = "servilla"
 
+import sys
+import os
+import ConfigParser
+import eml2_1_0.eml2_1_0
 
 def main():
+
+    config_path ='../conf/gmn_local_client.conf'
+    config = ConfigParser.ConfigParser()
+    config.read(config_path)
+
+    mp = config.get('Paths', 'metadata_path')
+    dp = config.get('Paths', 'data_path')
+    files = config.get('Files', 'metadata_files').split(',')
+
+    xml = open('/home/servilla/PycharmProjects/gmn_local_client/data/NIN/knb-lter-nin.0.1.xml').read()
+    eml = eml2_1_0.eml2_1_0.CreateFromDocument(xml)
+
+    ds = eml.dataset
+    ds_title = ds.title
+    ds_tbl = ds.dataTable
+    p = ds_tbl.physical
+
+    print(ds_tbl)
+
     return 0
 
 
