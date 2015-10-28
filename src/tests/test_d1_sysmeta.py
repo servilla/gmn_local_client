69983ab290f8854de73be86ba92b5181fda17016
Mark Servilla <mark.servilla@gmail.com>
2015-10-28 17:00:01 -0600

    Add conf section for unit tests.

---
+++ src/tests/test_d1_sysmeta.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: test_d1_sysmeta
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    10/28/15
+"""
+
+__author__ = 'servilla'
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
