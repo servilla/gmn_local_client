329189b6865aa56225b6a815a5d9051d59915a98
Mark Servilla <mark.servilla@gmail.com>
2015-10-28 17:02:19 -0600

    Add new unit test for generating D1 system metadata.

--- src/tests/test_d1_sysmeta.py
+++ src/tests/test_d1_sysmeta.py
@@ -14,12 +14,36 @@
 
 __author__ = 'servilla'
 
+from tests import config # This is effectively "from __init__ import config"
+
+import logging
+logger = logging.getLogger('tests.test_d1_sysmeta')
+logger.setLevel(logging.DEBUG)
+
 import unittest
+
+from eml_data_package import data_package
+from eml_data_package import data_table
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def setUp(self):
+        self.fn = config.get('Tests', 'test_file')
+        self.dp = data_package.DataPackage(self.fn)
+        self.dt = data_table.DataTable(self.dp.get_dataTable())
+        self.table_count = 1
+        self.n_table = 0
+        self.table_entityName = 'MeteorologicalData-NIN-LTER-1982-1982'
+        self.table_physical_count = 1
+        self.n_physical = 0
+        self.table_physical_objectName = 'LTER.NIN.MET.csv'
+
+    def tearDown(self):
+        pass
+
+    def test_file_name(self):
+        self.assertEqual(self.fn, self.fn)
 
 
 if __name__ == '__main__':
