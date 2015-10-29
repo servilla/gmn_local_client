2f61111b75040c27147833c69e8c8d42f7c9de8a
Mark Servilla <mark.servilla@gmail.com>
2015-10-29 14:40:22 -0600

    Add use of config properties.

--- src/tests/test_file_hash.py
+++ src/tests/test_file_hash.py
@@ -14,6 +14,8 @@
 
 __author__ = 'servilla'
 
+from tests import config
+
 import logging
 logger = logging.getLogger('tests.test_file_hash')
 logger.setLevel(logging.DEBUG)
@@ -25,7 +27,7 @@ from d1_actions.file_hash import file_hash
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.file_name = '/home/servilla/PycharmProjects/gmn_local_client/data/NIN/knb-lter-nin.0.1.xml'
+        self.fn = config.get('Tests', 'test_file')
         self.file_sha1 = '2b9dcb56e7bdf7057ead3160fdb9bbc952a1fc02'
         self.file_md5 = 'a10455a9623a90d49ae88de54cdbe5e1'
 
@@ -34,7 +36,7 @@ class MyTestCase(unittest.TestCase):
 
     def testFileSha1(self):
         try:
-            file_sha1 = file_hash(self.file_name, hash='sha1')
+            file_sha1 = file_hash(self.fn, hash='sha1')
         except ValueError as e:
             self.assertRaises(e)
         else:
@@ -42,7 +44,7 @@ class MyTestCase(unittest.TestCase):
 
     def testFileMd5(self):
         try:
-            file_md5 = file_hash(self.file_name, hash='md5')
+            file_md5 = file_hash(self.fn, hash='md5')
         except ValueError as e:
             self.assertRaises(e)
         else:
